c08ad570be8a650612185c89de3ae6256985aa64
jyguzman <jordieyguzman@gmail.com>
2024-04-21 10:55:27 -0500

    remove comments

--- JSONObject.py
+++ JSONObject.py
@@ -25,24 +25,9 @@ class JSONArray:
             self.items = []
 
     def add(self, *items: Union[JSONObject, 'JSONArray', str, float, int, None]):
-        # for item in items:
-        #     if isinstance(item, JSONObject):
-        #         self.items += item.entries
-        #     elif isinstance(item, JSONArray):
-        #         self.items += item.items
-        #     else:
         self.items += items
         return self
 
     def __repr__(self):
         return str(self.items)
-        #return '[' + ', '.join([str(item) for item in self.items]) + ']'
 
-
-if __name__ == "__main__":
-    json = (JSONObject()
-            .add("name", "Jordie")
-            .add("innerDict", JSONObject().add("innerItemKey", "innerItemVal"))
-            .add("age", 25)
-            .add("list", JSONArray().add(25, "drawing", "clashing")))
-    print(str(json))
